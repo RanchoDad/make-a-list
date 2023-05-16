751d8080f7eedc60e48060279f4a90a732027691
RanchoDad <joshuagarber215@gmail.com>
2023-05-16 10:25:19 -0700

    push

--- make_a_list/settings.py
+++ make_a_list/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b(qxv%^s%$4&83_155mdzx#tn09wtri*c6oxjx@@^2(&)8kk*1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://git.heroku.com/makealist.git']
 
 
 # Application definition
